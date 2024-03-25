229b22e50e6fc6366c862dbb03512d73450f70e8
Anay Sharma <115898324+AnaySharma01@users.noreply.github.com>
2024-03-24 21:45:40 -0400

    Update ImageProcessing.py

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -3,58 +3,63 @@ import cv2 as cv
 import numpy as np
 
 
-def processImage(frame):
+def processImage(image):
     # Applies gaussian blur, median blur, and canny edge detection on the image
     # https://github.com/adityagandhamal/road-lane-detection/blob/master/detection_on_vid.py Lines 35-38
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     gray_scale = cv.GaussianBlur(gray, (15, 15), 0)
     median_blur = cv.medianBlur(gray_scale, 5)
-    canny_image = cv.Canny(median_blur, 100, 20)
-    # https://www.geeksforgeeks.org/python-opencv-cv2-polylines-method/ #Lines 19-38
-    # Creates coordinates of mask
-    points = np.array([[800, 2100], [2300, 2100], [1800, 1500], [1600, 1500]],
-                      np.int32)
-    points = points.reshape((-1, 1, 2))
-    cv.polylines(frame, [points],
-                 True, (0, 255, 0), 5)
+    canny_image = cv.Canny(median_blur, 75, 20)
     # Creates a mask around desired area
     # https://pyimagesearch.com/2021/01/19/image-masking-with-opencv/ Lines 20-26
-    roi = np.zeros(frame.shape[:2], dtype="uint8")
-    cv.rectangle(roi, (1200, 1800), (2100, 2100), 1, -1)
+    roi = np.zeros(image.shape[:2], dtype="uint8")
+    cv.rectangle(roi, (100, 100), (400, 400), 1, -1)
     mask = cv.bitwise_and(canny_image, canny_image, mask=roi)
-
     # Displays the mask
-    # cv.rectangle(image, (1200, 1800), (2100, 2100), (255, 0, 0), 5)
+    # cv.rectangle(image, (100, 100), (400, 400), (255, 0, 0), 5)
 
-    # Creates hough lines around image
-    # Creates the hough lines used for the line detection
-    # https://github.com/adityagandhamal/road-lane-detection/blob/master/detection_on_vid.py Line 42
-    lines = cv.HoughLinesP(mask, 1, np.pi / 180, threshold=10, minLineLength=10, maxLineGap=15)
+    # Detects the contours
+    # https://www.tutorialspoint.com/opencv_python/opencv_python_image_contours.htm Line 10
+    contours, hierarchy = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
 
-    # Displays hough lines
-    # https://github.com/adityagandhamal/road-lane-detection/blob/master/detection_on_vid.py Line 14-19
-    # Prevents program from crashing if no lines detected
-    if lines is not None:
-        # Variables needed to find the centerline
-        slope_arr = []
-        lines_list = []
-        for line in lines:
-            # Creates array of lines
-            x1, y1, x2, y2 = line[0]
-            lines_list.append(line[0])
-            # Displays the lines
-            cv.line(frame, (x1, y1), (x2, y2), (0, 0, 255), 10)
-            # https://www.geeksforgeeks.org/program-find-slope-line/ Line 4
-            # Calculates the slopes of the lines
-            slope = 0
-            if x2 - x1 != 0:
-                slope = (y2 - y1) / (x2 - x1)
-            slope_arr.append(slope)
-        # https://www.geeksforgeeks.org/python-nested-loops/ Example 2 Lines 3 and 7
-        # Loops through the slope array to calculate the centerline
-        for i in range(len(slope_arr)):
-            for j in range(len(slope_arr)):
-                x1, y1, x2, y2 = lines_list[i]
-                x3, y3, x4, y4 = lines_list[j]
-                # Calculates and displays the centerline
-                cv.line(frame, ((x1 + x3) // 2, (y1 + y3) // 2), ((x2 + x4) // 2, (y2 + y4) // 2), (255, 0, 0), 5)
+    # Prevents program from crashing if no contours detected
+    if len(contours) > 0:
+        # Displays the contours
+        # https://www.tutorialspoint.com/opencv_python/opencv_python_image_contours.htm Line 15
+        cv.drawContours(image, contours, -1, (0, 255, 0), 5)
+        # Finds minimum length of contours
+        # https://www.geeksforgeeks.org/python-length-of-shortest-string-in-string-list/ Method 1 Line 13
+        min_length = min(len(cnt) for cnt in contours)
+
+        # Calculates the average of the contour points
+        # https://www.geeksforgeeks.org/numpy-mean-in-python/ Code 2 Line 15
+        midpoint_x_arr = np.mean([contour[:, 0, :][:min_length][:, 0] for contour in contours], axis=0).astype(int)
+        midpoint_y_arr = np.mean([contour[:, 0, :][:min_length][:, 1] for contour in contours], axis=0).astype(int)
+
+        # Displays the centerline
+        # https://www.geeksforgeeks.org/python-opencv-cv2-line-method/ Example 1 Line 33
+        for i in range(len(midpoint_x_arr) - 1):
+            cv.line(image, (midpoint_x_arr[i], midpoint_y_arr[i]), (midpoint_x_arr[i + 1], midpoint_y_arr[i + 1]),
+                    (0, 0, 255), 5)
+
+        # https://www.geeksforgeeks.org/draw-a-triangle-with-centroid-using-opencv/
+        # Sets the points of the arrows
+        point1 = (700, 900)
+        point2 = (1400, 1500)
+        point3 = (500, 1500)
+        # Draws the triangle with the points
+        cv.line(image, point1, point2, (255, 0, 0), 3)
+        cv.line(image, point2, point3, (255, 0, 0), 3)
+        cv.line(image, point1, point3, (255, 0, 0), 3)
+
+        # https://www.geeksforgeeks.org/python-opencv-cv2-arrowedline-method/?ref=lbp
+        # Creates the start and end point
+        start_point = (900, 1000)
+        end_point = (900, 200)
+        # Red color in BGR
+        color = (255, 0, 0)
+        # Sets the thickness of the arrow
+        thickness = 5
+        # Draws the arrow
+        cv.arrowedLine(image, start_point, end_point,
+                       color, thickness, tipLength=0.5)
