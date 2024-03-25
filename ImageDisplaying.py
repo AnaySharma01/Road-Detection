a00f34219b2d6c1de0c03c3f5317495e650d3948
Anay Sharma <115898324+AnaySharma01@users.noreply.github.com>
2024-03-24 21:59:53 -0400

    Update ImageDisplaying.py

--- ImageDisplaying.py
+++ ImageDisplaying.py
@@ -1,8 +1,8 @@
 #Imports necessary packages
 import cv2 as cv
 
-def displayImage(image):
+def displayImage(frame):
     #Returns the processed frame
     #https://www.geeksforgeeks.org/python-play-a-video-using-opencv/ lines 20 - 23
-    cv.imshow("Detected Lines", image)
+    cv.imshow("Detected Lines", frame)
     cv.waitKey(1)
